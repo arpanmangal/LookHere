08d263ab1515b6c46eaf595c8da5718f614b32d6
Arpan-sopho <mangalarpan@gmail.com>
2018-01-07 16:28:37 +0530

    updated plot function

--- Main Page/static/js/plot.js
+++ Main Page/static/js/plot.js
@@ -2,13 +2,46 @@
 // Call it using:
 // plot(Xdata, Yd)
 
-function plot(Xdata, Ydata, XLabel, Descrip) {
+console.log('hi');
+
+var XArray = [],
+    YArray = [],
+    XLabel = "test",
+    DescripArray = [];
+
+
+var completeXArray = [],
+    completeYArray = [],
+    completeDescripArray = [];
+
+/* Example Usage:
+  setInterval(function() {
+                plot(Math.random() * 10, Math.random() * 100, String((Math.random() * 50)));
+            }, 1000);
+*/
+
+function plot(Xdata, Ydata, Descrip) {
+    console.log(Xdata, Ydata, Descrip);
+    // Usage: pass a SINGLE Xdata, Ydata, Descrip
+    XArray.push(Xdata);
+    completeXArray.push(Xdata);
+    if (XArray.length > 20) XArray.shift(); // remove first entry
+
+    YArray.push(Ydata);
+    completeYArray.push(Ydata);
+    if (YArray.length > 20) YArray.shift();
+
+    DescripArray.push(Descrip);
+    completeDescripArray.push(Descrip);
+    if (DescripArray.length > 20) DescripArray.shift();
+
+
     var trace = {
-        x: Xdata,
-        y: Ydata,
+        x: XArray,
+        y: YArray,
         mode: 'markers',
-        name: 'You',
-        text: Descrip,//['United States', 'Canada'],
+        // name: 'You',
+        text: DescripArray,//['United States', 'Canada'],
         marker: {
           color: 'rgb(164, 194, 244)',
           size: 12,
@@ -24,6 +57,7 @@ function plot(Xdata, Ydata, XLabel, Descrip) {
 
     var layout = {
         title: 'Time',
+        showlegend: false,
         xaxis: {
           title: XLabel,
           showgrid: false,
