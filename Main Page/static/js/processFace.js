d6d139f1fe5d707b5a502237829a1e239d786149
Sushant101 <rathisushant5@gmail.com>
2018-01-07 13:46:43 +0530

    Auto-clicks photo and displays received JSON object

--- Main Page/static/js/processFace.js
+++ Main Page/static/js/processFace.js
@@ -3,105 +3,95 @@
 // pitch => down / top
 
 
-// function makeblob (dataURL) {
-//     // function to make the data object from dataURL encoding, to be sent in ajax
-//     var BASE64_MARKER = ';base64,';
-//     if (dataURL.indexOf(BASE64_MARKER) == -1) {
-//         var parts = dataURL.split(',');
-//         var contentType = parts[0].split(':')[1];
-//         var raw = decodeURIComponent(parts[1]);
-//         return new Blob([raw], { type: contentType });
-//     }
-//     var parts = dataURL.split(BASE64_MARKER);
-//     var contentType = parts[0].split(':')[1];
-//     var raw = window.atob(parts[1]);
-//     var rawLength = raw.length;
-//
-//     var uInt8Array = new Uint8Array(rawLength);
-//
-//     for (var i = 0; i < rawLength; ++i) {
-//         uInt8Array[i] = raw.charCodeAt(i);
-//     }
-//     var temp= new Blob([uInt8Array], { type: contentType });
-//     return temp;
-// }
-//
-// function processImage(dataURL, timestamp, callback) {
-//     // **********************************************
-//     // *** Update or verify the following values. ***
-//     // **********************************************
-//
-//     // Replace the subscriptionKey string value with your valid subscription key.
-//     var subscriptionKey = "4a8cf0860f0340cba3ec2b2aa7b4cab5";
-//
-//     // Replace or verify the region.
-//     //
-//     // You must use the same region in your REST API call as you used to obtain your subscription keys.
-//     // For example, if you obtained your subscription keys from the westus region, replace
-//     // "westcentralus" in the URI below with "westus".
-//     //
-//     // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
-//     // a free trial subscription key, you should not need to change this region.
-//     var uriBase = "https://southeastasia.api.cognitive.microsoft.com/face/v1.0/detect";
-//
-//     // Request parameters.
-//     var params = {
-//         "returnFaceId": "true",
-//         "returnFaceLandmarks": "false",
-//         "returnFaceAttributes": "headPose",
-//     };
-//
-//     // Display the image.
-//     var sourceImageUrl = document.getElementById("inputImage").value;
-//     document.querySelector("#sourceImage").src = sourceImageUrl;
-//
-//     // Perform the REST API call.
-//     $.ajax({
-//         url: uriBase + "?" + $.param(params),
-//
-//         type: 'POST',
-//         processData: false,
-//         contentType: 'application/octet-stream',
-//
-//
-//         // Request headers.
-//         beforeSend: function(xhrObj){
-//             // xhrObj.setRequestHeader("Content-Type","application/octet-stream");
-//             xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-//         },
-//
-//         data: makeblob(dataURL)
-//     })
-//
-//     .done(function(data) {
-//         var details;
-//
-//         if (data == null || data.length == 0) details = null;
-//         else {
-//             var faceAtt = data[0].faceAttributes;
-//             // Error handling
-//             if (faceAtt == null || !('headPose' in faceAtt)) {
-//                 // we did not receive the required data
-//                 details = null;
-//             } else {
-//                 details = faceAtt.headPose;
-//             }
-//         }
-//         var obj = {
-//             timestamp: timestamp,
-//             data: details
-//         }
-//         callback(obj);
-//     })
-//
-//     .fail(function(jqXHR, textStatus, errorThrown) {
-//         var obj = {
-//             timestamp: timestamp,
-//             data: null // change this to actual error message if you want err to be sent
-//         }
-//         callback(obj);
-//     });
-// };
+function makeblob (dataURL) {
+        var BASE64_MARKER = ';base64,';
+
+        if (dataURL.indexOf(BASE64_MARKER) == -1) {
+            var parts = dataURL.split(',');
+            var contentType = parts[0].split(':')[1];
+            var raw = decodeURIComponent(parts[1]);
+            return new Blob([raw], { type: contentType });
+        }
+        var parts = dataURL.split(BASE64_MARKER);
+        var contentType = parts[0].split(':')[1];
+        var raw = window.atob(parts[1]);
+        var rawLength = raw.length;
+
+        var uInt8Array = new Uint8Array(rawLength);
+
+        for (var i = 0; i < rawLength; ++i) {
+            uInt8Array[i] = raw.charCodeAt(i);
+        }
+        return new Blob([uInt8Array], {type: contentType});
+    }
+
+function processImage(dataURL) {
+        // **********************************************
+        // *** Update or verify the following values. ***
+        // **********************************************
+        console.log("Sending image to face api");
+        // Replace the subscriptionKey string value with your valid subscription key.
+        var subscriptionKey = "4a8cf0860f0340cba3ec2b2aa7b4cab5";
+
+        // Replace or verify the region.
+        //
+        // You must use the same region in your REST API call as you used to obtain your subscription keys.
+        // For example, if you obtained your subscription keys from the westus region, replace
+        // "westcentralus" in the URI below with "westus".
+        //
+        // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+        // a free trial subscription key, you should not need to change this region.
+        var uriBase = "https://southeastasia.api.cognitive.microsoft.com/face/v1.0/detect";
+
+        // Request parameters.
+        var params = {
+            "returnFaceId": "true",
+            "returnFaceLandmarks": "false",
+            "returnFaceAttributes": "headPose"
+        };
+
+        // Display the image.
+        //document.querySelector("#sourceImage").src = document.getElementById("inputImage").value;
+
+        // file=fopen("./test.jpg",0);
+        // str = fread(file,flength(file));
+
+
+        // Perform the REST API call.
+        $.ajax({
+            url: uriBase + "?" + $.param(params),
+
+            type: 'POST',
+            processData: false,
+            contentType: 'application/octet-stream',
+
+
+            // Request headers.
+            beforeSend: function(xhrObj){
+                // xhrObj.setRequestHeader("Content-Type","application/octet-stream");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+
+            // Request body.
+            // data: '{"url": ' + '"' + sourceImageUrl + '"}'
+            data: makeblob(dataURL)
+
+        })
+
+            .done(function(data) {
+                // Show formatted JSON on webpage.
+                console.log(JSON.stringify(data, null, 2));
+                //$("#responseTextArea").val(JSON.stringify(data, null, 2));
+            })
+
+            .fail(function(jqXHR, textStatus, errorThrown) {
+                // Display error message.
+                var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+                errorString += (jqXHR.responseText === "") ? "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
+                    jQuery.parseJSON(jqXHR.responseText).message : jQuery.parseJSON(jqXHR.responseText).error.message;
+                alert(errorString);
+            });
+    }
 
 
 function processFaces(file, timestamp, callback) {
@@ -121,6 +111,8 @@ function processFaces(file, timestamp, callback) {
     //                               console.log(obj); <= returned object
     //                          });
     // var input=file.src;
+    processImage(file, timestamp, callback);
+    return;
     var input = file.target;
     // console.log(input);
     console.log(file);
